29f7fa80b48e1c509b77b67f6050596d0c5bfd6a
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-01-11 23:09:22 +0000

    create a hidden layer

--- chapter_02.py
+++ chapter_02.py
@@ -9,8 +9,13 @@ def main():
                [0.5, -0.91, 0.26, -0.5],
                [-0.26, -0.27, 0.17, 0.87]]
     biases = [2, 3, 0.5]
-    layer_outputs = np.dot(inputs, np.array(weights).T) + biases
-    print(layer_outputs)
+    weights2 = [[0.1, -0.14, 0.5],
+                [-0.5, 0.12, -0.33],
+                [-0.44, 0.73, -0.13]]
+    biases2 = [-1, 2, -0.5]
+    layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+    layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+    print(layer2_outputs)
 
 
 if __name__ == '__main__':
