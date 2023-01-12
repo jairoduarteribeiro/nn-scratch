2723cf48663451774dfdedb55117a150cbb601b6
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-01-12 13:35:13 +0000

    create a dense layer

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import numpy as np
 import nnfs
-import matplotlib.pyplot as plt
 from nnfs.datasets import spiral_data
+from layer_dense import LayerDense
 
 nnfs.init()
 
 
 def main():
     x, y = spiral_data(samples=100, classes=3)
-    plt.scatter(x[:, 0], x[:, 1], c=y, cmap='brg')
-    plt.show()
+    dense1 = LayerDense(2, 3)
+    dense1.forward(x)
+    print(dense1.output[:5])
 
 
 if __name__ == '__main__':
