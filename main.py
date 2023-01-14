f3bcc615cc9a01749e934dce0404576157e53cd9
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-01-14 16:44:04 +0000

    create class ActivationSoftmax

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import nnfs
 from nnfs.datasets import spiral_data
 from layer_dense import LayerDense
-from activation import ActivationReLU
+from activation import ActivationReLU, ActivationSoftmax
 
 nnfs.init()
 
@@ -10,9 +10,13 @@ def main():
     x, y = spiral_data(samples=100, classes=3)
     dense1 = LayerDense(2, 3)
     activation1 = ActivationReLU()
+    dense2 = LayerDense(3, 3)
+    activation2 = ActivationSoftmax()
     dense1.forward(x)
     activation1.forward(dense1.output)
-    print(activation1.output[:5])
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+    print(activation2.output[:5])
 
 
 if __name__ == '__main__':
