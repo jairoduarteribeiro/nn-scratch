cb2c6ff4b524fd94039d357e183b9d27c84e0afb
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-01-14 23:41:31 +0000

    calculate the approximate derivative

--- derivatives.py
+++ derivatives.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 
 
 def f(x):
-    return 2 * x
+    return 2 * x**2
 
 
 def main():
-    x = np.array(range(5))
-    y = f(x)
-    plt.plot(x, y)
-    plt.show()
+    p2_delta = 0.0001
+    x1 = 1
+    x2 = x1 + p2_delta
+    y1 = f(x1)
+    y2 = f(x2)
+    approximate_derivative = (y2 - y1) / (x2 - x1)
+    print(approximate_derivative)
 
 
 if __name__ == '__main__':
