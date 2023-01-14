7dfdee5c84872297835b8d0fe8141d859368b91b
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-01-14 21:50:23 +0000

    print accuracy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import nnfs
+import numpy as np
 from nnfs.datasets import spiral_data
 from layer_dense import LayerDense
 from activation import ActivationReLU, ActivationSoftmax
@@ -21,6 +22,11 @@ def main():
     print(activation2.output[:5])
     loss = loss_function.calculate(activation2.output, y)
     print(f'loss: {loss}')
+    predictions = np.argmax(activation2.output, axis=1)
+    if len(y.shape) == 2:
+        y = np.argmax(y, axis=1)
+    accuracy = np.mean(predictions == y)
+    print(f'accuracy: {accuracy * 100:.2f}%')
 
 
 if __name__ == '__main__':
