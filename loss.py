591a17fab62e6acd6c5f5c895e63fe6a44b3bbd6
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2023-01-14 21:41:14 +0000

    create class LossCategoricalCrossEntropy

---
+++ loss.py
@@ -0,0 +1,27 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class Loss(ABC):
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+    @abstractmethod
+    def forward(self, y_pred, y_true):
+        pass
+
+
+class LossCategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        else:
+            raise RuntimeError('y_true must be a list of targets or one-hot vector')
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import nnfs
 from nnfs.datasets import spiral_data
 from layer_dense import LayerDense
 from activation import ActivationReLU, ActivationSoftmax
+from loss import LossCategoricalCrossEntropy
 
 nnfs.init()
 
@@ -12,11 +13,14 @@ def main():
     activation1 = ActivationReLU()
     dense2 = LayerDense(3, 3)
     activation2 = ActivationSoftmax()
+    loss_function = LossCategoricalCrossEntropy()
     dense1.forward(x)
     activation1.forward(dense1.output)
     dense2.forward(activation1.output)
     activation2.forward(dense2.output)
     print(activation2.output[:5])
+    loss = loss_function.calculate(activation2.output, y)
+    print(f'loss: {loss}')
 
 
 if __name__ == '__main__':
